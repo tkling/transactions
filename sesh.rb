efbbe1c8d47a6fdfc244fab7ce6db6f5fa59bd6d
tkling <tyling@gmail.com>
2021-02-16 00:05:41 -0500

    ivars instead of globals

--- sesh.rb
+++ sesh.rb
@@ -7,22 +7,22 @@ require 'transactions'
 require 'json'
 require 'pry'
 
-$start_time = Time.now
-$sets = []
+@start_time = Time.now
+@sets = []
 
 def new_ts(currency_type='whatever, man')
   TransactionSet.new(currency_type).tap do |ts|
-    $sets.push(ts)
+    @sets.push(ts)
   end
 end
 
 def save_sesh(force_new_filename: false)
   Dir.mkdir(SAVEFILE_DIR) unless Dir.exist?(SAVEFILE_DIR)
 
-  time      = (force_new_filename ? Time.now : $start_time).to_i
+  time      = (force_new_filename ? Time.now : @start_time).to_i
   filename  = "sesh_#{time}"
   file_path = File.join(SAVEFILE_DIR, filename)
-  content   = JSON.pretty_generate({start_time: time, sets: $sets.map(&:to_h)})
+  content   = JSON.pretty_generate({start_time: time, sets: @sets.map(&:to_h)})
 
   File.write(file_path, content)
 end
@@ -34,8 +34,8 @@ def load_last_sesh
   save_path = saves.sort.last
   from_json = JSON.parse(File.read(save_path), symbolize_names: true)
 
-  $start_time = Time.at(from_json[:start_time])
-  $sets = from_json[:sets].map {|ts_hash| TransactionSet.from_hash(ts_hash) }
+  @start_time = Time.at(from_json[:start_time])
+  @sets = from_json[:sets].map {|ts_hash| TransactionSet.from_hash(ts_hash) }
 end
 
 binding.pry
